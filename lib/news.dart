e759dfd19c7f687e8e5f78a93f600093377e48e9
Shashank25071999 <sahaishashank2507@gmail.com>
2020-06-17 05:06:39 +0530

    Changes required

--- lib/news.dart
+++ lib/news.dart
@@ -13,8 +13,38 @@ class NewsState extends State<News>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar:AppBar(title:Text("News"))
+   return Scaffold(
+      backgroundColor: Colors.white,
+      body:CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 85,
+            pinned: true,
+            floating: true,
+           backgroundColor:Colors.white,
+            flexibleSpace: FlexibleSpaceBar(
+               titlePadding: EdgeInsets.only(left:20,bottom:10,top: 20),
+              title: Text('News',style:TextStyle(color:Colors.black),),
+              
+            ),
+          ),
+          SliverFixedExtentList(itemExtent: 1750,
+            delegate: SliverChildListDelegate([
+             SingleChildScrollView(
+               child: Column(mainAxisAlignment: MainAxisAlignment.start,
+               crossAxisAlignment: CrossAxisAlignment.start,
+                 children: <Widget>[
+                                ],)
+               // Padding(padding: EdgeInsets.all(12.0),child: Text("You are"
+             )
+            ]))
+        ],
+      )
     );
   }
 }
+
+
+
+
+
